b2622663961e960d24dd797d8525a1a5b5c84076
ScottT4ylor <G00236985@smail.raritanval.edu>
2016-09-09 16:52:20 -0400

    Added Tree Movement and Dropping Apples
    Tree will move back and forth across the screen, also changing direction
    randomly and drop an apple every 2 seconds.

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class Apple : MonoBehaviour {
 
+	public static float killHeight= -20f;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		if (transform.position.y <= killHeight)
+		{
+			Destroy (this.gameObject);
+		}
 	}
 }
--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -1,15 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AppleTree : MonoBehaviour {
+public class AppleTree : MonoBehaviour
+{
+	public GameObject applePrefab;
+	public float speed = 2f;
+	public float leftAndRightEdge = 10f;
+	public float chanceToChangeDirection = 0.005f;
+	public float secondsBetweenAppleDrops = 1f;
+	private Vector3 currentPos;
+	private Vector3 newPos;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		InvokeRepeating ("dropApple", 2f, secondsBetweenAppleDrops);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update ()
+	{
+		currentPos = transform.position;
+		if (currentPos.x > leftAndRightEdge)
+		{
+			speed = -Mathf.Abs (speed);
+		}
+		else if (currentPos.x < -leftAndRightEdge)
+		{
+			speed = Mathf.Abs (speed);
+		}
+
+		newPos.Set (currentPos.x + speed * Time.deltaTime, currentPos.y, currentPos.z);
+		transform.position = newPos;
+	}
+	void FixedUpdate()
+	{
+		if (Random.value <= chanceToChangeDirection)
+		{
+			speed *= -1f;
+		}
+	}
+
+	void dropApple()
+	{
+		GameObject apple = Instantiate (applePrefab, transform.position, Quaternion.identity) as GameObject;
+		apple.transform.position = transform.position;
 	}
 }
