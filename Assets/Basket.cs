a237bddf680ceccc48654e4eaa8d8f1f4598a46a
ScottT4ylor <G00236985@smail.raritanval.edu>
2016-09-10 20:16:49 -0400

    Apple catching functional
    Baskets follow the mouse and any apples you catch are destroyed.

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -2,14 +2,32 @@
 using System.Collections;
 
 public class Basket : MonoBehaviour {
+    Vector3 mousePos2d;
+    Vector3 mousePos3d;
+    Vector3 pos;
+    GameObject hit;
 
-	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        mousePos2d = Input.mousePosition;
+        mousePos2d.z = 10; //Not going to dig through the main camera's transform's position, since it would vary if you moved the camera anyway.
+        mousePos3d = Camera.main.ScreenToWorldPoint(mousePos2d);
+        pos = transform.position;
+        pos.x = mousePos3d.x;
+        transform.position = pos;
 	}
+
+    void OnCollisionEnter(Collision coll)
+    {
+        hit = coll.collider.gameObject;
+        if (hit.CompareTag("Apple"))
+        {
+            Destroy(hit);
+        }
+    }
 }
